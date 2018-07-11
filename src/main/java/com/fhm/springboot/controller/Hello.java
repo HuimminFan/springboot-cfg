c6a673970073db5715bb1195c0d26239500304e3
tom_proglb <hello_glb@fhm.com>
2018-07-11 23:09:00 +0800

    xxx

--- src/main/java/com/fhm/springboot/controller/Hello.java
+++ src/main/java/com/fhm/springboot/controller/Hello.java
@@ -1,15 +1,25 @@
 package com.fhm.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Arrays;
+import java.util.Map;
+
+@Controller
 public class Hello {
     @Value("${person.name}")
     private String name;
     @RequestMapping("/sayHello")
     public String sayHello(){
         return "hell0" + name;
+    }
+
+    @RequestMapping("/success")
+    public String succ(Map<String,Object> maps){
+        maps.put("hello","<h1>你好</h1>");
+        maps.put("users", Arrays.asList("zhang","wang","li"));
+        return "/succ";
     }
 }
